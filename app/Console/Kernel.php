13f05e7a142f3f5c608d8dd5935f75d265b7782b
Daniel BC <danibeca@okazo.co>
2017-08-26 17:14:34 -0500

    Merge branch 'master' into fixes
    # Conflicts:
    #	composer.lock

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel {
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('indicatorscalculate')
-            ->hourly();
+        $schedule->command('indicator:calculate')
+            ->dailyAt('22:00');
     }
 }
